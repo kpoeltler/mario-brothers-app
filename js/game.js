84ea621dc92c42846efb1b15e8222b9dbacd84d3
kpoeltler <kpoeltler@gmail.com>
2018-09-10 20:50:34 -0500

    scaling

--- js/game.js
+++ js/game.js
@@ -1,42 +1,54 @@
-// create a scene object and pass it "Game"
-let gameScene = new Phaser.Scene('Game');
+// Initialize the game instance
 
+// window.myGame = window.myGame || {};
+
+// create a scene object and pass it "Game"
+let gameScene = new Phaser.Scene("Game");
+console.log(Phaser);
 // Load assets
 gameScene.preload = function() {
   // Load images,scene: background, players
-  this.load.image('background', 'assets/block.png');
-  this.load.image('player', 'assets/mushroom.png');
+  this.load.image("background", "assets/background.png");
+  this.load.image("player2", "assets/mushroom.png");
+  this.load.image("enemy", "assets/boo.png");
+  this.load.image("player1", 'assets/mario.png');
+
 };
 
 // called once after preload ends
-gameScene.create = function(){
-  
-  
+gameScene.create = function() {
   // create bg sprite
-  let bg = this.add.sprite(0, 0, 'background'); // set this to a variable so that you can change the origin
+  let bg = this.add.sprite(0, 0, "background"); // set this to a variable so that you can change the origin
 
   //change the origin to the top left corner, use the method setOrigin(0, 0)
-   bg.setOrigin(0, 0,);
+  bg.setOrigin(0, 0);
 
-   //create the player
-   let player = this.add.sprite(50, 180, 'player');
-   //change player position
-   player.x = 10
-   player.y = 20
-  
+  //change the background scale
+  bg.setScale(1);
+
+  //create the player sprites
+  let player2 = this.add.sprite(70, 250, "player2");
+  let player1 = this.add.sprite(120, 80, "player1");
+
+  //change player position
+  player2.setScale(0.2);
+  player1.setScale(.03);
+
+  //create the enemy sprite
+  let enemy = this.add.sprite(200, 190, 'enemy');
+  enemy.displayWidth = 120;
+  enemy.displayHeight = 180;
+
   // center the sprite
   // one way to do it is
-  bg.setPosition(640/2, 360/2);
-  
-  //change the background scale
-  bg.setScale(3, 3);
+  // bg.setPosition(640/2, 360/2);
 
   // another way to center the sprite is
-  let gameWidth = this.sys.game.config.width // this. refers to our gameScene object, sys. refers to Phaser's system, game. our object, config. is an object that includes everything, and dot width or dot height
-  let gameHeight = this.sys.game.config.height
+  let gameWidth = this.sys.game.config.width; // this. refers to our gameScene object, sys. refers to Phaser's system, game. our object, config. is an object that includes everything, and dot width or dot height
+  let gameHeight = this.sys.game.config.height;
   console.log(gameWidth, gameHeight);
   console.log(bg, "background image");
-  console.log(this, 'gameScene');
+  console.log(this, "gameScene");
 };
 
 // set the configuration of the game
